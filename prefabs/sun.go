034395d6efc391c870bbec528ea2f191da895e08
fortey <fortey@inbox.ru>
2026-05-03 19:16:02 +0700

    tutor and gameover

--- prefabs/sun.go
+++ prefabs/sun.go
@@ -6,7 +6,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "default_animation: \"sun\"\n"
-  "material: \"/resources/materials/crystal/crystal.material\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/assets/main.atlas\"\n"
